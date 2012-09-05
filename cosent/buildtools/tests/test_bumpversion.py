e39ea8820c48cccf78e7d096002a2e3b4832d61a
Guido Stevens <guido.stevens@cosent.nl>
2012-09-05 12:52:12 -0400

    cleanup import for better test readability

--- cosent/buildtools/tests/test_bumpversion.py
+++ cosent/buildtools/tests/test_bumpversion.py
@@ -2,32 +2,29 @@ import unittest
 import os
 import tempfile
 
-from cosent.buildtools.bumpversion import bump_rc
-from cosent.buildtools.bumpversion import bump_final
-from cosent.buildtools.bumpversion import get_version
-from cosent.buildtools.bumpversion import bump_setup_py
+from cosent.buildtools import bumpversion as bv
 
 
 class TestBumpVersion(unittest.TestCase):
 
     def test_rc2rc(self):
-        self.assertEquals(bump_rc('2.8rc1'), '2.8rc2')
-        self.assertEquals(bump_rc('2.8rc9'), '2.8rc10')
-        self.assertEquals(bump_rc('2.8rc19'), '2.8rc20')
-        self.assertEquals(bump_rc('2.8.rc19'), '2.8.rc20')
+        self.assertEquals(bv.bump_rc('2.8rc1'), '2.8rc2')
+        self.assertEquals(bv.bump_rc('2.8rc9'), '2.8rc10')
+        self.assertEquals(bv.bump_rc('2.8rc19'), '2.8rc20')
+        self.assertEquals(bv.bump_rc('2.8.rc19'), '2.8.rc20')
 
     def test_final2rc(self):
-        self.assertEquals(bump_rc('2.7'), '2.8rc1')
-        self.assertEquals(bump_rc('2.7.1'), '2.7.2rc1')
+        self.assertEquals(bv.bump_rc('2.7'), '2.8rc1')
+        self.assertEquals(bv.bump_rc('2.7.1'), '2.7.2rc1')
 
     def test_final2final(self):
-        self.assertEquals(bump_final('1'), '2')
-        self.assertEquals(bump_final('2.7'), '2.8')
-        self.assertEquals(bump_final('2.7.9'), '2.7.10')
+        self.assertEquals(bv.bump_final('1'), '2')
+        self.assertEquals(bv.bump_final('2.7'), '2.8')
+        self.assertEquals(bv.bump_final('2.7.9'), '2.7.10')
 
     def test_rc2final(self):
-        self.assertEquals(bump_final('2.8rc3'), '2.8')
-        self.assertEquals(bump_final('2.8.rc3'), '2.8')
+        self.assertEquals(bv.bump_final('2.8rc3'), '2.8')
+        self.assertEquals(bv.bump_final('2.8.rc3'), '2.8')
 
 
 class TestRewriteSetup(unittest.TestCase):
@@ -52,10 +49,10 @@ class TestRewriteSetup(unittest.TestCase):
         os.remove(self.temp_filename)
 
     def test_get_version(self):
-        self.assertEquals(get_version(self.dummy_setup.splitlines()), '0.1')
+        self.assertEquals(bv.get_version(self.dummy_setup.splitlines()), '0.1')
 
     def test_bump_setup_py_rc(self):
-        bump_setup_py(self.temp_filename)
+        bv.bump_setup_py(self.temp_filename)
         with open(self.temp_filename, 'r') as fh:
             new_setup = fh.read().splitlines()
         diff = []
@@ -66,7 +63,7 @@ class TestRewriteSetup(unittest.TestCase):
         self.assertEquals(diff, [("version = '0.1'", "version = '0.2rc1'")])
 
     def test_bump_setup_py_final(self):
-        bump_setup_py(self.temp_filename, final=True)
+        bv.bump_setup_py(self.temp_filename, final=True)
         with open(self.temp_filename, 'r') as fh:
             new_setup = fh.read().splitlines()
         diff = []
